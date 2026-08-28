73a9d75b84f04e47fc28bef5b6ffe06174526e6e
RLucas1102 <robenoltl@gmail.com>
2026-08-28 13:21:51 -0400

    Implemented ConstantField that inherits from Volume<T>; Added notes at the bottom

--- include/ImplicitFields.h
+++ include/ImplicitFields.h
@@ -2,19 +2,43 @@
 #define IMPLICITFIELDS_H
 
 #include "Volume.h"
+#include "Vector.h"
 
 namespace lux {
 
     template<typename T>
-    class ConstantVolume : public Volume<T> {
+    class ConstantField : public Volume<T> {
 
-        ConstantVolume();
-        ~ConstantVolume = default;
+        using typename Volume<T>::volumeDataType;
+        using typename Volume<T>::volumeGradType;
 
-        const volumeDataType eval(T v) const override
+        public:
+            ConstantField(const T v) : _value(v), _gradValue() {}
+            ~ConstantField() = default;
+
+            const volumeDataType eval(const Vector& P) const override { return _value; }
+            const volumeGradType grad(const Vector& P) const override { return {}; }
+        
+        private:
+            volumeDataType _value;
+            volumeGradType _gradValue;
 
     };
 
 } 
 
 #endif
+
+/***************************************************
+ * Notes:
+ * 
+ * using typename Volume<T>::volumeDataType;
+ * using typename Volume<T>::volumeGradType;
+ * 
+ * Compiler cannot assume that volumeDataType/volumeGradType are types
+ * Volume<T> is a dependent base class which means we cannout assume its type
+ * If it was Volume<float> that would be fine 
+ * because we now know that volumeDataType is float
+ * We create a local typedef of the type for convenient use here
+ * 
+ ***************************************************/
