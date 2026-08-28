49eb1658e133b83af98147181cfb76f87c27ecc9
RLucas1102 <robenoltl@gmail.com>
2026-08-28 14:53:05 -0400

    Lots of testing for FieldOperator and AddField

--- base/main.C
+++ base/main.C
@@ -57,21 +57,42 @@ int main() {
     std::shared_ptr<Volume<Vector>> Va2 = std::make_shared<ConstantField<Vector>>(Vector(1,1,1));
     std::shared_ptr<Volume<Vector>> Vb2 = std::make_shared<ConstantField<Vector>>(Vector(2,2,2));
 
-    // // Matrix volumes
+    // Matrix volumes
     std::shared_ptr<Volume<Matrix>> Ma2 = std::make_shared<ConstantField<Matrix>>(Matrix(3));
     std::shared_ptr<Volume<Matrix>> Mb2 = std::make_shared<ConstantField<Matrix>>(Matrix(4));
 
     // Evaluate scalar volumes
-    std::cout << a2->eval(Vector(3,3,3)) << std::endl;
-    std::cout << b2->eval(Vector(3,3,3)) << std::endl;
+    std::cout << a2->eval(Vector(3,3,3)) << std::endl; //4
+    std::cout << b2->eval(Vector(3,3,3)) << std::endl; //3
 
     // Evaluate vector volumes
-    std::cout << (Va2->eval(Vector(3,3,3))).__str__() << std::endl;
-    std::cout << (Vb2->eval(Vector(3,3,3))).__str__() << std::endl;
+    std::cout << (Va2->eval(Vector(3,3,3))).__str__() << std::endl; //(1,1,1)
+    std::cout << (Vb2->eval(Vector(3,3,3))).__str__() << std::endl; //(2,2,2)
 
-    // // Evaluate matrix volumes
-    std::cout << (Ma2->eval(Vector(3,3,3))).__str__() << std::endl;
-    std::cout << (Mb2->eval(Vector(3,3,3))).__str__() << std::endl;
+    // Evaluate matrix volumes
+    std::cout << (Ma2->eval(Vector(3,3,3))).__str__() << std::endl; //Matrix(3)
+    std::cout << (Mb2->eval(Vector(3,3,3))).__str__() << std::endl; //Matrix(4)
+
+    // Should not be able to instaniate a FieldOperator class directly
+    // FieldOperator<float> s;
+
+    // Create a scalar AddField
+    std::shared_ptr<Volume<float>> c2 = std::make_shared<AddField<float>>(a2, b2); 
+
+    // Create a vector AddField
+    std::shared_ptr<Volume<Vector>> Vc2 = std::make_shared<AddField<Vector>>(Va2, Vb2); 
+
+    // Create a matrix AddField
+    std::shared_ptr<Volume<Matrix>> Mc2 = std::make_shared<AddField<Matrix>>(Ma2, Mb2); 
+
+    // Evaluate added scalar volumes
+    std::cout << c2->eval(Vector(3,3,3)) << std::endl; //7
+
+    // Evaluate added vector volumes
+    std::cout << (Vc2->eval(Vector(3,3,3))).__str__() << std::endl; //(3,3,3)
+
+    // Evaluate added matrix volumes
+    std::cout << (Mc2->eval(Vector(3,3,3))).__str__() << std::endl; //Matrix(7)
 
     return 0;
 }
