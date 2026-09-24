6bdc69f083c65a9e582b19ee92da1871734e83ad
RLucas1102 <robenoltl@gmail.com>
2026-09-24 10:01:53 -0400

    A new file to test grid class that uses openvdb

---
+++ base/gridTest.C
@@ -0,0 +1,23 @@
+#include <openvdb/openvdb.h>
+
+#include <iostream>
+#include <memory.h>
+
+#include "VolumeGrid.h"
+#include "FieldInterface.h"
+
+using namespace lux;
+
+int main() {
+
+    std::shared_ptr<VolumeGrid> myVGrid = std::make_shared<VolumeGrid>();
+
+    myVGrid->init(openvdb::Coord(-10, -10, -10), openvdb::Coord(10, 10, 10), 1.0, -1000);
+
+    VSP<float> c = constant(4.0f);
+
+    myVGrid->stamp(c);
+
+    std::cout << myVGrid->triLerp(Vector(0,0,0)) << std::endl; // 4
+
+}
